b431c6b3145d3964ac119947b2a868d9520718ec
Vitalii <vnr34@mail.ru>
2020-10-28 11:16:22 +0400

    работа с JQuery

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.main_btna, .main_btn, a[href="#sheldure"]').on('click', function() {
+        $('.overlay').fadeIn(2000);
+        $('.modal').slideDown(2000);
+    });
+
+    $('.close').click(function() {
+        $('.overlay').fadeOut(2000);
+        $('.modal').slideUp(2000);
+    });
+
+
+
+});
